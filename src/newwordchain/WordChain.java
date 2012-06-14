13bd8fe959009ed9c9f770b30963d6badcf1b8e2
Tum <tum@abctech-thailand.com>
2012-06-14 14:04:56 +0700

    Init tree done

--- src/newwordchain/WordChain.java
+++ src/newwordchain/WordChain.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,43 +14,82 @@ import java.util.List;
  */
 public class WordChain {
 
-    private TreeNode wordTree;
-    List<String> wordList;
+    private TreeNode root;
+    private List<String> wordList;
+    private HashMap<String, String> usedWord = new HashMap<String, String>();
     private List<String> testList = new ArrayList<String>();
 
     public String findWordChain(String startWord, String endWord) {
         wordList = initData(startWord.length());
-        System.out.println("List size before : "+wordList.size());
+        System.out.println("List size before : " + wordList.size());
         initTree(startWord);
-        for(String s:testList) {
-            System.out.println(s);
-        }
-        System.out.println("List size after : "+wordList.size());
-
-        System.out.println("*************************************");
-        wordList = testList;
-        System.out.println("List size before : "+wordList.size());
-        initTree(startWord);
-        for(String s:testList) {
-            System.out.println(s);
-        }
-        System.out.println("List size after : "+wordList.size());
+//        completeTree(root);
+        printTree(root);
+        System.out.println("*********************************");
+        printLeaf(root);
+//        for (String s : testList) {
+//            System.out.println(s);
+//        }
+//        System.out.println("List size after : " + wordList.size());
+//
+//        System.out.println("*************************************");
+//        wordList = testList;
+//        System.out.println("List size before : " + wordList.size());
+//        initTree(startWord);
+//        for (String s : testList) {
+//            System.out.println(s);
+//        }
+//        System.out.println("List size after : " + wordList.size());
         return "";
     }
 
     private void initTree(String startWord) {
-        //wordTree = new TreeNode();
-            List<String> chainList = listAllChain(startWord);
-            for(String s : chainList) {
-                testList.add(s);
-                wordList.remove(s);
+        TreeNode rootNode = new TreeNode();
+        root = rootNode;
+        rootNode.setData(startWord);
+        List<String> chainList = listAllChain(startWord);
+        List<TreeNode> childNodes = new ArrayList<TreeNode>();
+        for (String w : chainList) {
+            TreeNode node = new TreeNode();
+            node.setData(w);
+            node.setParentNode(rootNode);
+            childNodes.add(node);
+        }
+        rootNode.setChilds(childNodes);
+
+    }
+
+    private void printLeaf(TreeNode root) {
+        if (root.getChilds() == null || root.getChilds().size() == 0) {
+            System.out.println("I am leaf : " + root.getData());
+        } else {
+            for (TreeNode node : root.getChilds()) {
+                printLeaf(node);
             }
+        }
+    }
+
+
+    private void printTree(TreeNode root) {
+        usedWord = new HashMap<String, String>();
+        if (root != null) {
+            System.out.println(root.getData());
+            List<TreeNode> childNodes = root.getChilds();
+            if (childNodes != null) {
+                for (TreeNode node : childNodes) {
+                    if (usedWord.get(node.getData()) == null) {
+                        printTree(node);
+                        usedWord.put(node.getData(), node.getData());
+                    }
+                }
+            }
+        }
     }
 
     private List<String> listAllChain(String parentWord) {
         List<String> chainList = new ArrayList<String>();
-        for(String word : wordList) {
-            if(isChainValid(parentWord, word)) {
+        for (String word : wordList) {
+            if (isChainValid(parentWord, word)) {
                 chainList.add(word);
             }
         }
